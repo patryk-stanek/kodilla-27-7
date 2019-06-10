b266325f48744ca86fcc09b34daaf6ed425fea87
Patryk Stanek <p.stanek3@gmail.com>
2019-06-11 00:11:34 +0200

    fixed variable

--- src/Comment.js
+++ src/Comment.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Comment = ({text, votes, id, thumbUp, thumbDown}) => 
     <li>
-        {text} <span>votes: {votes}</span> <button onClick={() => thumbUp({id})}>+</button> <button onClick={() => thumbDown({id})}>-</button>
+        {text} <span>votes: {votes}</span> <button onClick={() => thumbUp(id)}>+</button> <button onClick={() => thumbDown(id)}>-</button>
     </li>
 
 export default Comment;
